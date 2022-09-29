3456eb33fe0b3bbbd08bd79b674237696f234b94
Pritam Kininge <pritam.kininge@icicisecurities.com>
2022-09-29 19:38:06 +0530

    feat: added user controller

--- index.ts
+++ index.ts
@@ -2,15 +2,24 @@
 
 import express, { Express, Request, Response } from "express";
 import dotenv, { DotenvParseOutput } from "dotenv";
+import bodyParser from "body-parser";
 import logger from "./src/services/log.service";
+import user from "./src/controllers/user";
 
 const app: Express = express();
 const env: DotenvParseOutput | undefined = dotenv.config().parsed;
 
-// controllers declared
-app.get("/", (req: Request, res: Response) => {
-	res.send("Express + TypeScript Server");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// basic API to check health of services
+app.get("/api_health", (req: Request, res: Response) => {
+	res.status(200);
+	res.json({ message: "APIs working Fine!" });
 });
+
+// all controller routes
+app.use("/api/v1/user", user); // user router
 
 app.listen(env?.port, () =>
 	logger("info", `Express is running on port ${env?.port}`)
---
+++ src/controllers/user.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+import express, { Router } from "express";
+
+// objet created to use service
+const router: Router = express.Router();
+
+export default router;
